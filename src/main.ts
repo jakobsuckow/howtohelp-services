200b5db90d3e530dc71ab1dd6ddfd21bf8a2efb6
Jakob Suckow <jakob@martian.agency>
2021-05-29 19:58:05 +0200

    asd

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,10 @@ async function bootstrap() {
   });
   app.use(cookieParser());
   app.enableCors({
-    origin: ["http://localhost:3000", "https://howtohelp.guide"],
+    origin: [
+      "http://localhost:3000",
+      "https://howtohelp.guide,https://howtohelp-next-jfjzbc54b-jakobsuckow941.vercel.app",
+    ],
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe());
