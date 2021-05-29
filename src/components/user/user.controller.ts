fd4853cb0eed90c0251c86425fa1c1ed28866035
Jakob Suckow <jakob@martian.agency>
2021-05-29 20:14:21 +0200

    asd

--- src/components/user/user.controller.ts
+++ src/components/user/user.controller.ts
@@ -61,10 +61,10 @@ export class UserController {
     this.loggerService.log(`origin: ${origin}`);
 
     res
-      .set("Access-Control-Allow-Origin", [
-        "http://localhost:3000",
-        "https://howtohelp-next-jfjzbc54b-jakobsuckow941.vercel.app",
-      ])
+      .set(
+        "Access-Control-Allow-Origin",
+        "https://howtohelp-next-jfjzbc54b-jakobsuckow941.vercel.app"
+      )
       .cookie("accessToken", accessToken, {
         expires: new Date(new Date().getTime() + 60 * 1000 * 60 * 2),
         sameSite: "strict",
