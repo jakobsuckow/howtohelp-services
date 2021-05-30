76233bb05915f77de8dff36d59649d6995642f90
Jakob Suckow <jakob@martian.agency>
2021-05-30 17:54:27 +0200

    asd

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,10 @@ import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { LoggerService } from "./components/logger/logger.service";
 import { ValidationPipe } from "@nestjs/common";
 import * as cookieParser from "cookie-parser";
+import { NestExpressApplication } from "@nestjs/platform-express";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     logger: new LoggerService(),
     cors: true,
   });
@@ -22,6 +23,7 @@ async function bootstrap() {
     ],
     credentials: true,
   });
+  app.set("trust proxy", 1);
   app.useGlobalPipes(new ValidationPipe());
 
   app.setGlobalPrefix("api/v1");
