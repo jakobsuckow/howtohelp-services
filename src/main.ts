5319b06d9ee83b03d0bbf341d7bed8ca2762aabd
Jakob Suckow <jakob@martian.agency>
2021-05-31 09:48:56 +0200

    asd

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ import { LoggerService } from "./components/logger/logger.service";
 import { ValidationPipe } from "@nestjs/common";
 import * as cookieParser from "cookie-parser";
 import { NestExpressApplication } from "@nestjs/platform-express";
-const { shouldSendSameSiteNone } = require("should-send-same-site-none");
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
@@ -14,7 +13,6 @@ async function bootstrap() {
     cors: true,
   });
   app.use(cookieParser());
-  app.use(shouldSendSameSiteNone);
   app.enableCors({
     origin: [
       "http://localhost:3000",
